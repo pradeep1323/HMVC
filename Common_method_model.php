7db5ed737e2973b2af0234132984fec2ffd83fa8
pradeep1323 <41495949+pradeep1323@users.noreply.github.com>
2019-12-12 14:39:00 +0530

    Create Common_method_model.php

---
+++ Common_method_model.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Name:    Common Method Model
+ * Author:  Pradeep
+ *           pradeepraj110197@gmail.com
+ *
+ * Requirements: PHP5 or above
+ *
+ * @package    CodeIgniter-Common-query-Method
+ * @author     Er.Pradeep
+ * @link       http://github.com/benedmunds/CodeIgniter-Ion-Auth
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Ion_auth_model extends CI_Model
+{
+
+}
